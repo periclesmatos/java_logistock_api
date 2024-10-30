913db7e34ba8e916612e45c82432b2fb8e224f7e
Pericles Matos <spfcpericles@gmail.com>
2024-10-30 15:43:03 -0300

    projeto inicial

---
+++ src/main/java/com/logistock/LogiStockApplication.java
@@ -0,0 +1,13 @@
+package com.logistock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistock/LogiStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
